ff83fd97f344d47e3b313360e9795bdf05548069
esoft07 - Byron Alejandro Acuña Acurio <ex150298@g.unicamp.br>
2021-12-26 18:14:30 -0300

    Code to use left strip to remove http://

---
+++ Strings/left_strip_link.py
@@ -0,0 +1,6 @@
+link = "https://realpython.com/lessons/string-formatting/"
+link = link.lstrip('/:pths')
+print(link)
+link = "http://ssrealpython.com/lessons/string-formatting/"
+link = link.lstrip(':psth/')
+print(link)
